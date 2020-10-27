0b4ba03579722551ec132e60ba952b55cc015594
Vaibhav <vaibhavbirlavb@gmail.com>
2020-10-28 00:18:51 +0530

    Init

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -16,39 +16,40 @@ class _SearchPageState extends State<SearchPage> {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         //leading: Image(image: AssetImage("assets/delivery_boy.png"), width: 5, height: 5,),
-        title: Center(child: Text(
+        title: Center(
+            child: Text(
           "Track Shipment",
-          style: TextStyle(
-            color: Colors.black
-          ),
+          style: TextStyle(color: Colors.black),
         )),
         elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: Column(
+      body: Center(child: Column(
         //mainAxisAlignment: MainAxisAlignment.center,
         //crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Padding(
             padding: EdgeInsets.only(bottom: 20),
           ),
-          Container(child: TextFormField(
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              //icon: Icon(Icons.search),
-              //labelText: "Enter Tracking ID",
-              hintText: "Consignment ID",
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          Container(
+            child: TextFormField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                //icon: Icon(Icons.search),
+                //labelText: "Enter Tracking ID",
+                hintText: "Consignment ID",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              ),
+              style: TextStyle(fontSize: 20),
+              onChanged: (text) {
+                trackId = text;
+              },
             ),
-            style: TextStyle(fontSize: 20),
-            onChanged: (text){
-              trackId = text;
-            },
-          ), width: MediaQuery.of(context).size.width * 0.6,
-          ),//width: MediaQuery.of(context).size.width * 0.6,),
+            width: MediaQuery.of(context).size.width * 0.6,
+          ), //width: MediaQuery.of(context).size.width * 0.6,),
 
-          
           Padding(
             padding: EdgeInsets.only(bottom: 20),
           ),
@@ -56,10 +57,9 @@ class _SearchPageState extends State<SearchPage> {
             color: Colors.blue,
             child: Text("TRACK"),
             onPressed: () {
-              if(trackId != null){
+              if (trackId != null) {
                 var route = new MaterialPageRoute(
-                  builder: (context) => TrackPage(cnote: trackId)
-                );
+                    builder: (context) => TrackPage(cnote: trackId));
                 Navigator.of(context).push(route);
               }
               // setState(() {
@@ -67,30 +67,23 @@ class _SearchPageState extends State<SearchPage> {
               // });
             },
           ),
-          
-          Padding(
-            padding: EdgeInsets.only(bottom: 50)
-          ),
-          
+
+          Padding(padding: EdgeInsets.only(bottom: 50)),
+
           Expanded(
-            child: Image(
-              image: AssetImage("assets/process.jpg"),
-            )
-          )
-          
+              child: Image(
+            image: AssetImage("assets/process.jpg"),
+          ))
         ],
+      ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title: Text("Track")
-          ),
+              icon: Icon(Icons.search), title: Text("Track")),
           BottomNavigationBarItem(
-            icon: Icon(Icons.location_city),
-            title: Text("Branch Locator")
-          )
+              icon: Icon(Icons.location_city), title: Text("Branch Locator"))
         ],
       ),
     );
--- lib/screens/track.dart
+++ lib/screens/track.dart
@@ -11,8 +11,10 @@ import 'package:skyking_tracking/classes/contackinfo_class.dart';
 //387236300
 
 Future fetchPost(String cnote) async {
-  final response =
-      await http.get('https://live.skyking.co/api/Track/ConsignmentMTrack_Mobile?true&cnote=' + cnote + '&key=skm&Email=skm%40flyking.co.in',
+  final response = await http.get(
+      'https://live.skyking.co/api/Track/ConsignmentMTrack_Mobile?true&cnote=' +
+          cnote +
+          '&key=skm&Email=skm%40flyking.co.in',
       headers: {"Accept": "application/json"});
   return response.body;
 }
@@ -25,8 +27,9 @@ Future<List<Checkpoint>> loadCheckpoints(String cnote) async {
 }
 
 Future fetchInfo(String id) async {
-  final response =
-      await http.get('https://live.skyking.co/FlykingTransaction.asmx/GetBranchDetailsForTrack?ID=' + id,
+  final response = await http.get(
+      'https://live.skyking.co/FlykingTransaction.asmx/GetBranchDetailsForTrack?ID=' +
+          id,
       headers: {"Accept": "application/json"});
   //print(response.body);
   return response.body;
@@ -35,21 +38,18 @@ Future fetchInfo(String id) async {
 Future<ContactInfo> loadInfo(String id) async {
   String jsonString = await fetchInfo(id);
   final jsonResponse = json.decode(jsonString);
-  
+
   ContactInfo info = ContactInfo.fromJson(jsonResponse[0]);
   return info;
 }
 
 Future podLink(String id) async {
-  final response =
-      await http.get('https://live.skyking.co/api/values/TrachPdfDisplay?vchDRSNo=' + id,
+  final response = await http.get(
+      'https://live.skyking.co/api/values/TrachPdfDisplay?vchDRSNo=' + id,
       headers: {"Accept": "application/json"});
   final jsonResponse = json.decode(response.body);
   return jsonResponse[0]["DRS_ScanCopy"];
 }
-
-
-
 
 class TrackPage extends StatefulWidget {
   final String cnote;
@@ -72,121 +72,117 @@ class _TrackPageState extends State<TrackPage> {
   Widget makeDialog(String id) {
     return FutureBuilder(
       future: loadInfo(id),
-      builder: (context, snapshot){
+      builder: (context, snapshot) {
         return AlertDialog(
           elevation: 0,
           title: Center(child: Text("Contact Info")),
-          content: snapshot.hasData ?
-          Container(child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              CupertinoButton(
-                color: Colors.green[100],
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.location_on, 
-                    color: Colors.black), 
-                    Padding(padding: EdgeInsets.only(left: 5)),
-                    Text("Open In Maps", 
-                      style: TextStyle(color: Colors.black, fontSize: 14), 
-                    )
-                  ]
-                ),
-                onPressed: () => launch("https://www.google.com/maps/search/?api=1&query=" + snapshot.data.address),
-              ),
-              CupertinoButton(
-                color: Colors.blue[100],
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.call, color: Colors.black), 
-                    Padding(padding: EdgeInsets.only(left: 5)),
-                    Text(snapshot.data.contactNo.split(" ")[0], 
-                      style: TextStyle(color: Colors.black, fontSize: 14)
-                    )
-                  ],
-                ),
-                onPressed: () => launch("tel:" + snapshot.data.contactNo.split(" ")[0]),
-              ),
-              CupertinoButton(
-                color: Colors.blue[100],
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.call, color: Colors.black), 
-                    Padding(padding: EdgeInsets.only(left: 5)),
-                    Text(snapshot.data.contactNo.split(" ")[1], 
-                      style: TextStyle(color: Colors.black, fontSize: 14)
-                    )
-                  ],
-                ),
-                onPressed: () => launch("tel:" + snapshot.data.contactNo.split(" ")[1]),
-              ),
-              CupertinoButton(
-                color: Colors.red[100],
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.mail_outline, 
-                    color: Colors.black), 
-                    Padding(padding: EdgeInsets.only(left: 5)),
-                    Text("Email", 
-                      style: TextStyle(color: Colors.black, fontSize: 14)
-                    )
-                  ]
-                ),
-                onPressed: () => launch("mailto:" + snapshot.data.eMail),
-              ),
-            ]
-          ), height: 300)
-          : LinearProgressIndicator(),
+          content: snapshot.hasData
+              ? Container(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        CupertinoButton(
+                          color: Colors.green[100],
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(Icons.location_on, color: Colors.black),
+                                Padding(padding: EdgeInsets.only(left: 5)),
+                                Text(
+                                  "Open In Maps",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 14),
+                                )
+                              ]),
+                          onPressed: () => launch(
+                              "https://www.google.com/maps/search/?api=1&query=" +
+                                  snapshot.data.address),
+                        ),
+                        CupertinoButton(
+                          color: Colors.blue[100],
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.call, color: Colors.black),
+                              Padding(padding: EdgeInsets.only(left: 5)),
+                              Text(snapshot.data.contactNo.split(" ")[0],
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 14))
+                            ],
+                          ),
+                          onPressed: () => launch(
+                              "tel:" + snapshot.data.contactNo.split(" ")[0]),
+                        ),
+                        CupertinoButton(
+                          color: Colors.blue[100],
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.call, color: Colors.black),
+                              Padding(padding: EdgeInsets.only(left: 5)),
+                              Text(snapshot.data.contactNo.split(" ")[1],
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 14))
+                            ],
+                          ),
+                          onPressed: () => launch(
+                              "tel:" + snapshot.data.contactNo.split(" ")[1]),
+                        ),
+                        CupertinoButton(
+                          color: Colors.red[100],
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(Icons.mail_outline, color: Colors.black),
+                                Padding(padding: EdgeInsets.only(left: 5)),
+                                Text("Email",
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 14))
+                              ]),
+                          onPressed: () =>
+                              launch("mailto:" + snapshot.data.eMail),
+                        ),
+                      ]),
+                  height: 300)
+              : LinearProgressIndicator(),
         );
       },
     );
   }
 
-  List<Step> getSteps(BuildContext context, AsyncSnapshot snapshot)  {
+  List<Step> getSteps(BuildContext context, AsyncSnapshot snapshot) {
     List<Step> steps = [];
-    
-    for(int i = 0; i < snapshot.data.length; i++){
-      Step s = Step(
-        title: Container(
-          child: Text(
-            snapshot.data[i].status
-          ),
-          width: MediaQuery.of(context).size.width * 0.75
-        ),
-        subtitle: Text("Time: " + snapshot.data[i].dateTime),
-        isActive: true, 
 
-        content: Container(
-          child: Column(
+    for (int i = 0; i < snapshot.data.length; i++) {
+      Step s = Step(
+          title: Container(
+              child: Text(snapshot.data[i].status),
+              width: MediaQuery.of(context).size.width * 0.75),
+          subtitle: Text("Time: " + snapshot.data[i].dateTime),
+          isActive: true,
+          content: Container(
+              child: Column(
             children: <Widget>[
-              Text(
-                "Location: " + snapshot.data[i].location + "\n",
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold
-                )
-              ),
+              Text("Location: " + snapshot.data[i].location + "\n",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
               CupertinoButton(
                 color: Colors.grey[350],
-                child: Text("View Details", style: TextStyle(color: Colors.black)),
+                child:
+                    Text("View Details", style: TextStyle(color: Colors.black)),
                 onPressed: () {
                   showDialog(
-                    context: context,
-                    child: snapshot.data[i].branchId == "0" ?
-                    AlertDialog(title: Text("Conact Info"), content: Text("Info Not Available"),)
-                    : makeDialog(snapshot.data[i].branchId)
-                  );
+                      context: context,
+                      child: snapshot.data[i].branchId == "0"
+                          ? AlertDialog(
+                              title: Text("Conact Info"),
+                              content: Text("Info Not Available"),
+                            )
+                          : makeDialog(snapshot.data[i].branchId));
                 },
               ),
             ],
-          )
-        )
-      );
+          )));
       steps.add(s);
     }
     return steps;
@@ -194,7 +190,6 @@ class _TrackPageState extends State<TrackPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       /*
       appBar: AppBar(
@@ -202,68 +197,77 @@ class _TrackPageState extends State<TrackPage> {
       ),
       */
       appBar: AppBar(
-        title: Text("Tracking Results", style: TextStyle(color: Colors.black),),
+        title: Text(
+          "Tracking Results",
+          style: TextStyle(color: Colors.black),
+        ),
         elevation: 0,
         backgroundColor: Colors.white,
-        iconTheme: IconThemeData(
-          color: Colors.black
-        ),
+        iconTheme: IconThemeData(color: Colors.black),
       ),
       body: FutureBuilder<List<Checkpoint>>(
-        future: loadCheckpoints(widget.cnote),
-        builder: (context, snapshot){
-          if(snapshot.hasData){
-            int len = snapshot.data.length;
-            String s = snapshot.data[len - 1].statusflag;
-            if(len == 0){
-              return Center(child: Image(image: AssetImage("assets/404.jpg")));
-
-            }else{
-              if(s == "1"){
-                return Column(
-                  children: <Widget>[
-                    Container(
-                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[Text("DELIVERED"), CupertinoButton(
-                        child: Text("View POD", style: TextStyle(color: Colors.black),),
-                        color: Colors.blue[100],
-                        onPressed: () => showpdf(snapshot.data[len - 1].refNo),
-                      ),]
-                    ),),
-                    Stepper(
-                      steps: getSteps(context, snapshot),
-                      currentStep: _current,
-                      controlsBuilder: (context, {onStepContinue, onStepCancel}){
-                        return Row();
-                      },
-                      onStepTapped: (int i) {
-                        setState(() {
-                          _current = i;
-                        });
-                      },
-                    )
-                  ],
-                );
-              }else{
-                return Stepper(
-                  steps: getSteps(context, snapshot),
-                  currentStep: _current,
-                  controlsBuilder: (context, {onStepContinue, onStepCancel}){
-                    return Row();
-                  },
-                  onStepTapped: (int i) {
-                    setState(() {
-                      _current = i;
-                    });
-                  },
-                );
+          future: loadCheckpoints(widget.cnote),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              int len = snapshot.data.length;
+              String s = snapshot.data[len - 1].statusflag;
+              if (len == 0) {
+                return Center(
+                    child: Image(image: AssetImage("assets/404.jpg")));
+              } else {
+                if (s == "1") {
+                  return Column(
+                    children: <Widget>[
+                      Container(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Text("DELIVERED"),
+                              CupertinoButton(
+                                child: Text(
+                                  "View POD",
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                                color: Colors.blue[100],
+                                onPressed: () =>
+                                    showpdf(snapshot.data[len - 1].refNo),
+                              ),
+                            ]),
+                      ),
+                      Stepper(
+                        steps: getSteps(context, snapshot),
+                        currentStep: _current,
+                        controlsBuilder: (context,
+                            {onStepContinue, onStepCancel}) {
+                          return Row();
+                        },
+                        onStepTapped: (int i) {
+                          setState(() {
+                            _current = i;
+                          });
+                        },
+                      )
+                    ],
+                  );
+                } else {
+                  return Stepper(
+                    steps: getSteps(context, snapshot),
+                    currentStep: _current,
+                    controlsBuilder: (context, {onStepContinue, onStepCancel}) {
+                      return Row();
+                    },
+                    onStepTapped: (int i) {
+                      setState(() {
+                        _current = i;
+                      });
+                    },
+                  );
+                }
               }
+            } else {
+              return LinearProgressIndicator();
             }
-          }else{
-            return LinearProgressIndicator();
-          }
-        }
-      ),
+          }),
     );
   }
 }
